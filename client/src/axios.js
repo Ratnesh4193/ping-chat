26549ac3e2c24b7671ffff691920828a52d7a4c0
Ratnesh <rktssgk@gmail.com>
2021-12-24 12:06:32 +0530

    updated .

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import dotenv from "dotenv";
 
+dotenv.config(); //for accessing env variables
+
 const PORT = process.env.PORT || 8000;
 console.log(PORT);
 const instance = axios.create({
