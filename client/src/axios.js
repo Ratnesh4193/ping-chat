0bde02e0a5b81f3b65abda4c908586f28d55b029
Ratnesh <rktssgk@gmail.com>
2021-12-24 11:51:10 +0530

    updated .

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
-const instance =axios.create({
-    baseURL:"http://localhost:8000"
-})
+const instance = axios.create({
+	baseURL: "https://localhost:8000",
+});
 
-export default instance
+export default instance;
