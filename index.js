db8827604b9d5d03db72219df34828e534fbe4c7
Ratnesh <rktssgk@gmail.com>
2021-12-24 11:03:57 +0530

    updated .

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 8000;
 
 if (process.env.NODE_ENV == "production") {
 	app.use(express.static("client/build"));
-	const path = require("path");
+	import path from "path";
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
