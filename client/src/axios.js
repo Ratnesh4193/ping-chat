2fdd747279adbcdb3f251b7ffc1c8a670f866449
Ratnesh <rktssgk@gmail.com>
2021-12-24 12:02:51 +0530

    updated .

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
 const PORT = process.env.PORT || 8000;
+console.log(PORT);
 const instance = axios.create({
 	baseURL: `https://localhost:${PORT}`,
 });
